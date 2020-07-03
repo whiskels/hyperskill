1312b52d4997730031dcf330dfa1e9a3d0c19963
whiskels <kuzmingeo@gmail.com>
2020-07-03 11:16:03 +0300

    Readability score clean up

--- Readability Score/Readability Score/task/src/readability/Main.java
+++ Readability Score/Readability Score/task/src/readability/Main.java
@@ -1,7 +1,5 @@
 package readability;
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args)  {
         ReadabilityEstimator readabilityEstimator = new ReadabilityEstimator(args[0]);
